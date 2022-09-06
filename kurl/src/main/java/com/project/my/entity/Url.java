da475e0c3c0cf1dc8d3a1ca88d8bef783dd0f1fd
Mohsen Mirzaei <mohsen.mirzaei@dfreight.org>
2022-09-06 15:06:25 +0430

    added created date column using JPA Auditing.

--- kurl/src/main/java/com/project/my/entity/Url.java
+++ kurl/src/main/java/com/project/my/entity/Url.java
@@ -2,9 +2,13 @@ package com.project.my.entity;
 
 import com.project.my.configuration.Constants;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.Date;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Data
 @NoArgsConstructor
@@ -22,5 +26,10 @@ public class Url {
 
     @Column(nullable = false, unique = true, length = Constants.SHORT_URL_MAX_LENGTH)
     private String shortUrl;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, updatable = false)
+    private Date createdDate;
 
 }
